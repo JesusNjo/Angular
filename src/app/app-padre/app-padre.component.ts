d7c5827c5cdc52b069d00d7452e2ece2beebd410
Francisco Naranjo <fnaranjo@jacidi.com>
2024-01-30 15:23:47 -0400

    cambios

--- src/app/app-padre/app-padre.component.ts
+++ src/app/app-padre/app-padre.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app-padre.component.css']
 })
 export class AppPadreComponent {
+  tittle: string = "Angular"
+  numberClick: number = 0;
+  //Para enviar el mensaje al hijo
+  datoEntrada: String = "-------------";
 
-}
+  mensaje: string = "";
+  recibirMensaje($event:string){
+    this.mensaje = $event;
+  }
+
+  incrementar(){
+    this.numberClick++;
+  }
+
+  decrementar(): void {
+    this.numberClick ===0 ? this.numberClick : this.numberClick--;
+  }
+}  
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppPadreComponent } from './app-padre/app-padre.component';
+import { AppHijoComponent } from './app-padre/app-hijo/app-hijo.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppPadreComponent
+    AppPadreComponent,
+    AppHijoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+    
   ],
   providers: [],
   exports:  [],
